efed5e6f6e08a489d7e985428eba6c0879a159c8
adrian bermejo <adrianbermejogom@gmail.com>
2017-11-08 12:17:31 +0100

    casi funciona falta lo de aumentar los meses y años al llegar a dia 31

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,77 +1,47 @@
 
 public class CalendarioBasico
 {
-	// declarando variables dia mes y año
-	private int dia;
-	private int mes;
-	private int ano;
+    // declarando variables dia mes y año
+    private DisplayDosCaracteres dia;
+    private DisplayDosCaracteres mes;
+    private DisplayDosCaracteres ano;
 
-		/**
-		*constructor de clase CalendarioBasico
-		*/
+        /**
+        *constructor de clase CalendarioBasico
+        */
 
-	public CalendarioBasico()
-	{
-		dia = 1;
-		mes = 1;
-		ano = 1;
-	}
-	/**
-	*Fijamos fecha nueva
-	*/
-		public void fijarFecha(int nuevoDia, int nuevoMes,int nuevoAnio)
-	{
-		dia = nuevoDia;
-		mes = nuevoMes;
-		ano = nuevoAnio;
-	}
-	/**
-	*metodo para obtener la fecha
-	*/
-	public String obtenerFecha()
-	{
-		String textoADevolver = "";
-		String parte1 = dia + "";
-		String parte2 = mes + "";
-		String parte3 = ano + "";
-		if (parte1.length() < 2) {
-		parte1 = "0" +parte1;
-		}
-		if (mes< 10) {
-		parte2 = "0" + parte2;
-		}
-		if (ano < 10) {
-		parte3 = "0" + parte3;
-		}
-		textoADevolver = parte1 + "-" + parte2 + "-" +parte3;
-		return textoADevolver;
-	}
+    public CalendarioBasico()
+    {
+        DisplayDosCaracteres dia = new DisplayDosCaracteres(31);
+        DisplayDosCaracteres mes = new DisplayDosCaracteres(13);
+        DisplayDosCaracteres ano = new DisplayDosCaracteres(2099);
+    }
+    /**
+    *Fijamos fecha nueva
+    */
+        public void fijarFecha(int nuevoDia, int nuevoMes,int nuevoAnio)
+    {
+        dia.setValorAlmacenado (nuevoDia);
+        mes.setValorAlmacenado (nuevoMes);
+        ano.setValorAlmacenado (nuevoAnio);
+    }
+    /**
+    *metodo para obtener la fecha
+    */
+    public String obtenerFecha()
+    {
+      
+        return  dia.getTextoDelDisplay()+ "-"+ mes.getTextoDelDisplay()+ "-" + ano.getTextoDelDisplay();
+    }
 
-	/**
-	*avanzar un dia en la fecha
-	*/
-	public void avanzarFecha()
-	{
-	dia = dia + 1;
-		if (dia == 31) {
-		dia = 1;
-		mes = mes + 1;
-		if (mes == 13) {
-			ano = ano + 1;
-			mes = 1;
-			}
-		}
-	}
-
-
-
-
-
-
-
-
-
-
+    /**
+    *avanzar un dia en la fecha
+    */
+   public void avanzarFecha()
+    {
+        dia.incrementaValorAlmacenado();
+    }
 
 
 }
+
